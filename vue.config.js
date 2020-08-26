eeb34066dc640a4ad14f26839e8e2178a5a20c94
longchun <longchun@ctf.com.cn>
2020-08-26 17:49:43 +0800

    home1

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// 配置别名
+module.exports = {
+    configureWebpack: {
+        reslove: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}

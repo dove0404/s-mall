76a03869fa9188c9c401ccc2cb8a0b51b37083aa
longchun <longchun@ctf.com.cn>
2020-08-26 17:47:39 +0800

    home

---
+++ src/components/TabBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="s-mall-tabbar">
+    <div class="s-mall-item"><i></i><span>首页</span></div>
+    <div class="s-mall-item"><i></i><span>分类</span></div>
+    <div class="s-mall-item"><i></i><span>购物车</span></div>
+    <div class="s-mall-item"><i></i><span>我的</span></div>
+  </div>
+</template>
+
+<style scoped>
+#s-mall-tabbar{
+  display: flex;
+  background-color: aquamarine;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.s-mall-item{
+  flex: 1;
+  text-align: center;
+  border: 1px solid #666;
+  height: 49px;
+}
+.s-mall-item i{
+  display: block;
+  height: 20px;
+  width: 20px;
+  background-color: blueviolet;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <tab-bar/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TabBar from '@/components/TabBar'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    TabBar
   }
 }
 </script>
+
+<style scoped>
+
+</style>
